1db106f5768c8081b37768f7e1161336ba7314a9
Gabriel Morris <gabemorris@outlook.com>
2018-09-18 10:52:31 -0700

    Added mongo

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  PORT: process.env.PORT || 8080
+  PORT: process.env.PORT || 8080,
+  MONGODB_URL: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful'
 };
